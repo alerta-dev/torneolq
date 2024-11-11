70ff44cc5cd9f41122f46e2d04355ad27f7be5ca
AlertDev <49847759+alerta-dev@users.noreply.github.com>
2024-11-11 07:09:40 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include("includes/header.php"); ?>
+<div class="inicio">
+    <div class="background">
+        <h1>¡Bienvenidos al TorneoLq!</h1>
+        <p>Un torneo de creadores donde la creatividad es el límite.</p>
+    </div>
+</div>
