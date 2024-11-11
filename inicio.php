4ad15f1fe905ff4ff3ca8d88fa999c138d802e6f
AlertDev <49847759+alerta-dev@users.noreply.github.com>
2024-11-11 07:21:21 -0300

    Update and rename ranking.php to inicio.php

---
+++ inicio.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio | TorneoLq</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body class="inicio">
+    <div class="content">
+        <h1>Bienvenidos al TorneoLq</h1>
+        <h2>El lugar donde los mejores creadores de video compiten</h2>
+    </div>
+</body>
+</html>
