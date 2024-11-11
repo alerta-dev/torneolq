8818a5de21127c84d93297853ccf65337d4a7c58
AlertDev <49847759+alerta-dev@users.noreply.github.com>
2024-11-11 07:12:47 -0300

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TorneoLq</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="ranking.php">Ranking</a></li>
+            <li><a href="registro.php">Registro</a></li>
+        </ul>
+    </nav>
+</body>
